b2da9ade19b2d8e130b57fb49e061013f39d6b00
zD0n <dedmap0526@gmail.com>
2026-03-05 19:08:58 +0700

    Create deploy.sh

---
+++ database/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd ./
+
+git fetch origin Ignoreme
+
+LOCAL_HASH=$(git rev-parse HEAD)
+REMOTE_HASH=$(git rev-parse origin/Ignoreme)
+
+if [ "$LOCAL_HASH" != "$REMOTE_HASH" ]; then
+    echo "New commits detected. Deploying..."
+    
+    git pull origin Ignoreme
+    
+    docker compose down
+    docker compose up --build -d
+    
+    echo "Deployment complete."
+else
+    echo "No new commits. Skipping."
+fi
