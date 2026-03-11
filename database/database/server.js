3036637146cf77b5f24d12bfba56e1210a307896
zD0n <dedmap0526@gmail.com>
2026-03-11 12:16:08 +0700

    Test1

--- database/database/server.js
+++ database/database/server.js
@@ -35,10 +35,6 @@ app.get("/health", async (req, res) => {
   res.status(200).json({ status: "Online" })
 })
 
-app.get("/health2", async (req, res) => {
-  res.status(200).json({ status: "Online" })
-})
-
 // Get all tables
 app.get("/tables", async (req, res) => {
   try {
